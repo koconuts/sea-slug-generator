a9020f47541fa22033ba43d1188407c09f1d8168
Natsuho Kimura <kimnatsu0815@gmail.com>
2020-04-12 00:40:44 +0900

    部分セレクタ追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    <div class="header">
-      <h1>うみうしジェネレータ</h1>
+    <header class="header">
+      <h1 class="title">うみうしジェネレータ</h1>
+    </header>
+    <div class="container">
+      <img class="main-img" src="./assets/images/slug_1.png">
     </div>
-    <img class="main-img" src="./assets/images/slug_1.png">
+    <div class="container">
+      <ul class="selector part-selector">
+        <!-- 選択状態 -->
+        <li class="item part-item selected">
+          <h2 class="part-item-name selected">からだ</h2>
+        </li>
+        <!-- 未選択状態 -->
+        <li class="item part-item">
+          <h2 class="part-item-name">頭触手</h2>
+        </li>
+        <li class="item part-item">
+          <h2 class="part-item-name">二次鰓</h2>
+        </li>
+        <li class="item part-item">
+          <h2 class="part-item-name">もよう</h2>
+        </li>
+        <li class="item part-item">
+          <h2 class="part-item-name">ふち</h2>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
