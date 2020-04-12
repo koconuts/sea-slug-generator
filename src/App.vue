d22aa1d128ecddac260235c958894e0334c8755e
Natsuho Kimura <kimnatsu0815@gmail.com>
2020-04-12 17:40:44 +0900

    完成ボタン追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header class="header">
-      <h1 class="title">うみうしジェネレータ</h1>
+      <h1 class="text title">うみうしジェネレータ</h1>
     </header>
     <div class="body">
       <div class="left-container">
@@ -10,101 +10,106 @@
         </div>
       </div>
       <div class="right-container">
-        <div class="container">
-          <ul class="selector part-selector">
-            <!-- 選択状態 -->
-            <li class="item part-item selected">
-              <h2 class="part-item-name selected">からだ</h2>
-            </li>
-            <!-- 未選択状態 -->
-            <li class="item part-item">
-              <h2 class="part-item-name">ぱーつ</h2>
-            </li>
-            <li class="item part-item">
-              <h2 class="part-item-name">もよう</h2>
-            </li>
-            <li class="item part-item">
-              <h2 class="part-item-name">背景</h2>
-            </li>
-          </ul>
+        <div class="selectors">
+          <div class="container">
+            <ul class="selector part-selector">
+              <!-- 選択状態 -->
+              <li class="item part-item selected">
+                <h2 class="text part-item-name selected">からだ</h2>
+              </li>
+              <!-- 未選択状態 -->
+              <li class="item part-item">
+                <h2 class="text part-item-name">ぱーつ</h2>
+              </li>
+              <li class="item part-item">
+                <h2 class="text part-item-name">もよう</h2>
+              </li>
+              <li class="item part-item">
+                <h2 class="text part-item-name">背景</h2>
+              </li>
+            </ul>
+          </div>
+          <div class="selector-heading-container">
+              <h2 class="text selector-heading">頭触手</h2>
+          </div>
+          <div class="container">
+            <ul class="selector shape-selector">
+              <!-- 選択状態 -->
+              <li class="item shape-item selected">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+              <!-- 未選択状態 -->
+              <li class="item shape-item">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+              <li class="item shape-item">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+              <li class="item shape-item">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+            </ul>
+          </div>
+          <div class="container">
+            <ul class="selector color-selector">
+              <!-- 選択状態 -->
+              <li class="item color-item selected"></li>
+              <!-- 未選択状態 -->
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+            </ul>
+            <ul class="selector color-selector">
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+            </ul>
+          </div>
+          <div class="selector-heading-container">
+              <h2 class="text selector-heading">二次鰓</h2>
+          </div>
+          <div class="container">
+            <ul class="selector shape-selector">
+              <!-- 選択状態 -->
+              <li class="item shape-item selected">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+              <!-- 未選択状態 -->
+              <li class="item shape-item">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+              <li class="item shape-item">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+              <li class="item shape-item">
+                <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
+              </li>
+            </ul>
+          </div>
+          <div class="container">
+            <ul class="selector color-selector">
+              <!-- 選択状態 -->
+              <li class="item color-item selected"></li>
+              <!-- 未選択状態 -->
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+            </ul>
+            <ul class="selector color-selector">
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+              <li class="item color-item"></li>
+            </ul>
+          </div>
         </div>
-        <div class="selector-heading-container">
-            <h2 class="selector-heading">頭触手</h2>
-        </div>
-        <div class="container">
-          <ul class="selector shape-selector">
-            <!-- 選択状態 -->
-            <li class="item shape-item selected">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-            <!-- 未選択状態 -->
-            <li class="item shape-item">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-            <li class="item shape-item">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-            <li class="item shape-item">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-          </ul>
-        </div>
-        <div class="container">
-          <ul class="selector color-selector">
-            <!-- 選択状態 -->
-            <li class="item color-item selected"></li>
-            <!-- 未選択状態 -->
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-          </ul>
-          <ul class="selector color-selector">
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-          </ul>
-        </div>
-        <div class="selector-heading-container">
-            <h2 class="selector-heading">二次鰓</h2>
-        </div>
-        <div class="container">
-          <ul class="selector shape-selector">
-            <!-- 選択状態 -->
-            <li class="item shape-item selected">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-            <!-- 未選択状態 -->
-            <li class="item shape-item">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-            <li class="item shape-item">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-            <li class="item shape-item">
-              <img class="shape-item-img" src="./assets/images/parts/slug-parts_1.png">
-            </li>
-          </ul>
-        </div>
-        <div class="container">
-          <ul class="selector color-selector">
-            <!-- 選択状態 -->
-            <li class="item color-item selected"></li>
-            <!-- 未選択状態 -->
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-          </ul>
-          <ul class="selector color-selector">
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-            <li class="item color-item"></li>
-          </ul>
+        <div class="complete-btn">
+          <p class="text complete-btn-text">かんせい</p>
         </div>
       </div>
     </div>
