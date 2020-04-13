6f7861a2d70986d1b3c47afcfddfcc0eba0b0356
Natsuho Kimura <kimnatsu0815@gmail.com>
2020-04-13 22:01:39 +0900

    color-selector ラジオボタン化

--- src/components/Parts.vue
+++ src/components/Parts.vue
@@ -21,20 +21,26 @@
     </div>
     <div class="container">
       <ul class="selector color-selector">
-        <!-- 選択状態 -->
-        <li class="item color-item selected"></li>
-        <!-- 未選択状態 -->
-        <li class="item color-item"></li>
-        <li class="item color-item"></li>
-        <li class="item color-item"></li>
-        <li class="item color-item"></li>
+        <li class="item color-item" v-for="(item, key) in colorSelectorItems" :key="key">
+          <input
+            :id=item.id
+            type="radio"
+            name="color-item"
+            checked
+          >
+          <label :for=item.id class="text color-item-label"></label>
+        </li>
       </ul>
       <ul class="selector color-selector">
-        <li class="item color-item"></li>
-        <li class="item color-item"></li>
-        <li class="item color-item"></li>
-        <li class="item color-item"></li>
-        <li class="item color-item"></li>
+        <li class="item color-item" v-for="(item, key) in colorSelectorItems2" :key="key">
+          <input
+            :id=item.id
+            type="radio"
+            name="color-item"
+            checked
+          >
+          <label :for=item.id class="text color-item-label"></label>
+        </li>
       </ul>
     </div>
     <div class="selector-heading-container">
@@ -91,7 +97,21 @@ export default {
         {id: 'radio_img2'},
         {id: 'radio_img3'},
         {id: 'radio_img4'}
-      ]
+      ],
+      colorSelectorItems: [
+        {id: 'radio_color1'},
+        {id: 'radio_color2'},
+        {id: 'radio_color3'},
+        {id: 'radio_color4'},
+        {id: 'radio_color5'}
+      ],
+      colorSelectorItems2: [
+        {id: 'radio_color6'},
+        {id: 'radio_color7'},
+        {id: 'radio_color8'},
+        {id: 'radio_color9'},
+        {id: 'radio_color10'}
+      ],
     }
   }
 }
