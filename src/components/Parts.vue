ac4629d1d236e1ffe940c8f39191c4482483a44c
Natsuho Kimura <kimnatsu0815@gmail.com>
2020-04-19 13:44:46 +0900

    親からshapeSelectorItemsを渡す

--- src/components/Parts.vue
+++ src/components/Parts.vue
@@ -3,11 +3,19 @@
     <div class="selector-heading-container">
       <h2 class="text selector-heading">頭触手</h2>
     </div>
-    <shape-selector></shape-selector>
+    <shape-selector
+      :shapeSelectorItems="shapeSelectorItemsToushokushu"
+      :name="toushokushu"
+    ></shape-selector>
     <color-selector></color-selector>
-    <!-- <div class="selector-heading-container">
+    <div class="selector-heading-container">
       <h2 class="text selector-heading">二次鰓</h2>
-    </div> -->
+    </div>
+    <shape-selector
+      :shapeSelectorItems="shapeSelectorItemsNijihai"
+      :name="nijihai"
+    ></shape-selector>
+    <color-selector></color-selector>
   </div>
 </template>
 
@@ -21,5 +29,23 @@ export default {
     ShapeSelector,
     ColorSelector,
   },
+  data() {
+    return {
+      toushokushu: 'toushokushu',
+      nijihai: 'nijihai',
+      shapeSelectorItemsToushokushu: [
+        {id: 'radio_img1'},
+        {id: 'radio_img2'},
+        {id: 'radio_img3'},
+        {id: 'radio_img4'}
+      ],
+      shapeSelectorItemsNijihai: [
+        {id: 'radio_img5'},
+        {id: 'radio_img6'},
+        {id: 'radio_img7'},
+        {id: 'radio_img8'}
+      ],
+    }
+  }
 }
 </script>
--- src/components/ShapeSelector.vue
+++ src/components/ShapeSelector.vue
@@ -3,9 +3,10 @@
     <ul class="selector shape-selector">
       <li class="item shape-item" v-for="(item, key) in shapeSelectorItems" :key="key">
         <input
+          class="shape-item-input"
           :id=item.id
           type="radio"
-          name="shape-item"
+          :name="name"
           :checked=isChecked(key)
         >
         <label :for=item.id class="text shape-item-label">
@@ -21,21 +22,25 @@ export default {
   name: 'ShapeSelector',
   components: {
   },
+  props: {
+    shapeSelectorItems: Array,
+    name: String,
+  },
   methods: {
     isChecked(key) {
       return key === 0 ? 'checked' : '';
     }
   },
-  data() {
-    return {
-      shapeSelectorItems: [
-        {id: 'radio_img1'},
-        {id: 'radio_img2'},
-        {id: 'radio_img3'},
-        {id: 'radio_img4'}
-      ],
-    }
-  }
+  // data() {
+    // return {
+    //   shapeSelectorItems: [
+    //     {id: 'radio_img1'},
+    //     {id: 'radio_img2'},
+    //     {id: 'radio_img3'},
+    //     {id: 'radio_img4'}
+    //   ],
+    // }
+  // }
 }
 </script>
 
