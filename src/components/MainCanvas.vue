6317c57f12945b58c514e3c92bed21599baee654
Natsuho Kimura <kimnatsu0815@gmail.com>
2020-05-02 20:45:34 +0900

    URLの共通部分を説明変数にする

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -16,14 +16,15 @@ export default {
       const canvas = this.$refs.canvas
       canvas.height = canvas.width;
       const ctx = canvas.getContext('2d');
+      const baseUrl = 'https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/'
 
       let srcs = {
-        body: `https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/body_${color[0].replace('radio_color_body' , '')}.png`,
-        toushokushu: `https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/toushokushu_1_${color[1].replace('radio_color_toushokushu' , '')}.png`,
-        nijiera: `https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/nijiera_1_${color[2].replace('radio_color_nijiera' , '')}.png`,
-        surface: `https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/pattern_1_${color[3].replace('radio_color_surface' , '')}.png`,
-        side: `https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/side_${color[4].replace('radio_color_side' , '')}.png`,
-        bg: `https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/bg_${color[5].replace('radio_color_bg' , '')}.png`,
+        body: `${baseUrl}body_${color[0].replace('radio_color_body', '')}.png`,
+        toushokushu: `${baseUrl}toushokushu_1_${color[1].replace('radio_color_toushokushu', '')}.png`,
+        nijiera: `${baseUrl}nijiera_1_${color[2].replace('radio_color_nijiera', '')}.png`,
+        surface: `${baseUrl}pattern_1_${color[3].replace('radio_color_surface', '')}.png`,
+        side: `${baseUrl}side_${color[4].replace('radio_color_side', '')}.png`,
+        bg: `${baseUrl}bg_${color[5].replace('radio_color_bg', '')}.png`,
       };
 
       const bodyImg = new Image();
