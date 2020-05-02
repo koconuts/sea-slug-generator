9d486a82f7163ea866b1914d142d253873bbdb92
Natsuho Kimura <kimnatsu0815@gmail.com>
2020-05-02 21:23:21 +0900

    画像の重なり順を固定する

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -18,50 +18,29 @@ export default {
       const ctx = canvas.getContext('2d');
       const baseUrl = 'https://sea-slug-generator.s3-ap-northeast-1.amazonaws.com/img/'
 
-      let srcs = {
-        body: `${baseUrl}body_${color[0].replace('radio_color_body', '')}.png`,
-        toushokushu: `${baseUrl}toushokushu_1_${color[1].replace('radio_color_toushokushu', '')}.png`,
-        nijiera: `${baseUrl}nijiera_1_${color[2].replace('radio_color_nijiera', '')}.png`,
-        surface: `${baseUrl}pattern_1_${color[3].replace('radio_color_surface', '')}.png`,
-        side: `${baseUrl}side_${color[4].replace('radio_color_side', '')}.png`,
-        bg: `${baseUrl}bg_${color[5].replace('radio_color_bg', '')}.png`,
-      };
+      const srcs = [
+        `${baseUrl}bg_${color[5].replace('radio_color_bg', '')}.png`,
+        `${baseUrl}body_${color[0].replace('radio_color_body', '')}.png`,
+        `${baseUrl}toushokushu_1_${color[1].replace('radio_color_toushokushu', '')}.png`,
+        `${baseUrl}nijiera_1_${color[2].replace('radio_color_nijiera', '')}.png`,
+        `${baseUrl}pattern_1_${color[3].replace('radio_color_surface', '')}.png`,
+        `${baseUrl}side_${color[4].replace('radio_color_side', '')}.png`,
+      ];
 
-      const bodyImg = new Image();
-      bodyImg.onload = function() {
-        ctx.drawImage(bodyImg, 0, 0, canvas.width, canvas.height);
-      };
-      bodyImg.src = srcs.body;
-
-      const toushokushuImg = new Image();
-      toushokushuImg.onload = function() {
-        ctx.drawImage(toushokushuImg, 0, 0, canvas.width, canvas.height);
-      };
-      toushokushuImg.src = srcs.toushokushu;
-
-      const nijieraImg = new Image();
-      nijieraImg.onload = function() {
-        ctx.drawImage(nijieraImg, 0, 0, canvas.width, canvas.height);
-      };
-      nijieraImg.src = srcs.nijiera;
-
-      const surfaceImg = new Image();
-      surfaceImg.onload = function() {
-        ctx.drawImage(surfaceImg, 0, 0, canvas.width, canvas.height);
-      };
-      surfaceImg.src = srcs.surface;
-
-      const sideImg = new Image();
-      sideImg.onload = function() {
-        ctx.drawImage(sideImg, 0, 0, canvas.width, canvas.height);
-      };
-      sideImg.src = srcs.side;
-
-      // const bgImg = new Image();
-      // bgImg.onload = function() {
-      //   ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
-      // };
-      // bgImg.src = srcs.bg;
+      const imgs = [];
+      let count = 0;
+      for (var index = 0; index < srcs.length; index++) {
+        imgs[index] = new Image();
+        imgs[index].src = srcs[index];
+        imgs[index].onload = () => {
+          count++;
+          if (count >= srcs.length) {
+            for (var i = 0; i < imgs.length; i++) {
+              ctx.drawImage(imgs[i], 0, 0, canvas.width, canvas.height);
+            }
+          }
+        }
+      }
     },
   },
   mounted() {
